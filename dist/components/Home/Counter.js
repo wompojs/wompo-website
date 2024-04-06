6180a58687f2a7723824aef5814edf7a90689155
Lorenzo Lannino <lanninolore@gmail.com>
2024-04-06 23:24:09 +0200

    Fixed import

--- dist/components/Home/Counter.js
+++
@@ -1,8 +0,0 @@
-import{defineWompo as n,html as r,useState as u}from"wompo";export default function t(){const[e,o]=u(0);return r`<button @click=${()=>o(e+1)}>${e}</button>`}t.css=`
-  button {
-    padding: 3rem;
-    font-size: 4rem;
-    border-radius: 10px;
-    cursor: pointer;
-  }
-`,n(t,{shadow:!0,cssModule:!1});
